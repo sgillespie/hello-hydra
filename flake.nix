72b9d3df26a81e7778f70e1db8db1851604e9f62
Sean D Gillespie <sean@mistersg.net>
2024-10-18 15:33:44 -0400

    Add a brutally simple flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.hello;
+      hydraJobs.${system}.packages.default = pkgs.hello;
+    };
+}
